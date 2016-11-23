8a2c910fc6bd504109bdbae6a2d84741a25eef2e
Anna <annakrygier@hotmail.com>
2016-11-23 14:17:53 -0800

    Refactor ammaps geocoding

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -66,7 +66,7 @@ var work = {
 	}, {
 			'employer': 'Web Development Services LLC',
 			'title': 'visual designer',
-			'location': '',
+			'location': 'Larkspur, CA',
 			'locationInfo': '<img src=images/fort-lauderdale.jpg><p>Fort Lauderdale is a city on Florida\'s southeastern coast, known for its beaches and miles of boating canals.</p>',
 			'dates': '2013 - 2015',
 			'description': '<div class="work-info"><p>Marketing oriented web and graphic designer:</p><ul><li>marketing emails, banners, landing pages, UI elements, branding materials</li><li>design strategy creation and deployment oversight</li></ul></div>'
@@ -104,13 +104,13 @@ var projects = {
 		'companyUrl': 'http://avantspa.com',
 		'dates': '',
 		'description': '',
-		'images': ['images/avs-2.jpg', 'images/avs-1.jpg']
+		'images': ['images/AVS-2.jpg', 'images/AVS-1.jpg']
 	}, {
 		'title': 'Interpid Partners Consulting',
 		'companyUrl': 'http://intrepid-partners-consulting.com',
 		'dates': '',
 		'description': '',
-		'images': ['images/ipc-2.jpg', 'images/ipc-1.jpg']
+		'images': ['images/IPC-2.jpg', 'images/IPC-1.jpg']
 	}],
 	'display': function () {
 		projects.projects.forEach(projects.addProjectInfo);
@@ -201,7 +201,7 @@ var myModal = {
 		geocoder.geocode({
 			'location': latlng
 		}, function (results, status) {
-			address = results[0].address_components[2].long_name;
+			let address = results[0].address_components[2].long_name;
 			myModal.modalMessage(address);
 		});
 	},
@@ -224,9 +224,6 @@ work.display();
 education.display();
 projects.display();
 
-// reference var
-var coordinates, description, label, locationsInfo, places;
-
 
 // TODO: defer writing until screen width reached
 if ($(window).width() > 599) {
@@ -247,13 +244,9 @@ if ($(window).width() > 599) {
 		'map': 'worldLow',
 		'images': []
 	};
-	myMap.display = function (addr, label) {
+	myMap.display = function (addr, label, info) {
 		lat = addr.lat();
 		lng = addr.lng();
-		description = myMap.descriptionFinder(work.jobs, label);
-		if (description === undefined) {
-			description = myMap.descriptionFinder(education.schools, label);
-		}
 		myMap.dataProvider.images.push({
 			"type": 'circle',
 			"labelPosition": 'middle',
@@ -265,76 +258,50 @@ if ($(window).width() > 599) {
 			"longitude": lng,
 			"label": label,
 			"title": label,
-			"description": description
+			"description": info
 		});
 		myMap.write('map');
 	};
-	myMap.coordinate = function (addresses, descriptions) {
-		var geoResult = function () {
+	myMap.checkDuplicate = function (element, index, array) {
+		return element !== array[index - 1];
+	}
+	myMap.coordinate = function (locations) {
+		var geoResult = function (location) {
+			let address = location.address;
+			let description = location.description;
 			geocoder.geocode({
-				'address': addresses[i]
+				'address': address
 			}, function (results, status) {
-				coordinates = results[0].geometry.location;
-				address = results[0].formatted_address;
-				myMap.display(coordinates, address);
+				let coordinates = results[0].geometry.location,
+					address = results[0].formatted_address;
+				myMap.display(coordinates, address, description);
 			});
 		};
-		for (var i = 0, j = addresses.length; i < j; i++) {
-			geoResult();
+		for (var i = 0, j = locations.length; i < j; i++) {
+			geoResult(locations[i]);
 		}
-	};
-
-	myMap.descriptionFinder = function (data, label) {
-		for (var i = 0, j = data.length; i < j; i++) {
-			var str2 = label.trim();
-			var str1 = data[i].location.trim();
-			if (str1.substr(0, 3).toLowerCase() === str2.substr(0, 3).toLowerCase()) {
-				description = data[i].locationInfo;
-				return description;
-			}
-		}
-		return description;
 	};
 	myMap.locationFinder = function () {
-		places = [];
-		var schools = education.schools;
-		var jobs = work.jobs;
-		for (var key in schools) {
-			if (schools.hasOwnProperty(key)) {
-				places.push(schools[key].location);
+		let places = [];
+		var aggregatedPlaces = education.schools.concat(work.jobs);
+		for (var key in aggregatedPlaces) {
+			if (aggregatedPlaces.hasOwnProperty(key)) {
+				let place = {
+					address: aggregatedPlaces[key].location,
+					description: aggregatedPlaces[key].locationInfo
+				}
+				places.push(place);
 			}
 		}
-		for (key in jobs) {
-			if (jobs.hasOwnProperty(key)) {
-				places.push(jobs[key].location);
-			}
-		}
-		return places;
-	};
-	myMap.locationInfoFinder = function () {
-		places = [];
-		var schools = education.schools;
-		var jobs = work.jobs;
-		for (var key in schools) {
-			if (schools.hasOwnProperty(key)) {
-				places.push(schools[key].locationInfo);
-			}
-		}
-		for (key in jobs) {
-			if (jobs.hasOwnProperty(key)) {
-				places.push(jobs[key].locationInfo);
-			}
-		}
-		return places;
-	};
+		places = places.sort().filter(myMap.checkDuplicate);
+		return places
+	}();
 	myMap.placement = function () {
 		$('#mapDiv').append('<div id="map"></div>');
 		$('#mapDiv h3').text('my life map');
-	};
-
-	myMap.placement();
+	}();
 	AmCharts.ready(function () {
-		myMap.coordinate(myMap.locationFinder(), myMap.locationInfoFinder());
+		myMap.coordinate(myMap.locationFinder);
 	});
 	myModal.display();
 }
